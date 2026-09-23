1c0f85e58982ac1fca1ed1d52ec029bb574ffeec
wolfice2208 <wolf.ice2208@gmail.com>
2026-09-23 13:50:52 +0300

    wqe

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-console.log("nigga");
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from config import FRONTEND_URL
+from routers import items
+
+app = FastAPI()
+
+app.add_middleware(
+    allow_origins=[FRONTEND_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(items.router)
+
+@app.get("/")
+def root():
+    return {"message" : "Welcome to the FastAPI application"}
